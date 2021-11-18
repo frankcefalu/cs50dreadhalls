e6630eb8f9fb7838561ad0ab397a4821643f3a03
Frank Cefalu <frankcefalu@gmail.com>
2021-11-17 22:10:18 -0500

    Initial

---
+++ Assets/Scripts/DiedOnFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DiedOnFall : MonoBehaviour
+{
+ 
+
+    private void Start()
+    {
+        Transform transform = GetComponent<Transform>();
+    }
+    
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -7)
+        {
+      
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/Scripts/GrabPickups.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GrabPickups : MonoBehaviour {
+
+	private AudioSource pickupSoundSource;
+	public static int level = 1;
+	public Text levelText;
+
+	void Awake() {
+		pickupSoundSource = DontDestroy.instance.GetComponents<AudioSource>()[1];
+	}
+
+
+    private void Update()
+    {
+		levelText.text = level.ToString();
+	}
+
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+		if (hit.gameObject.tag == "Pickup") {
+			pickupSoundSource.Play();
+			SceneManager.LoadScene("Play");
+			level++;
+		}
+	}
+}
